d6e88e88d89d76cf5aaa400d055971e0e25a3aae
canolidev <canovoti@gmail.com>
2026-05-18 17:22:54 -0500

    friday

--- js/script.js
+++ js/script.js
@@ -233,3 +233,46 @@ console.log(`10 * 2 = ${fifthResult}`);
 
 const sixthResult = 0 + 22;
 console.log(`0 + 22 = ${sixthResult}`);
+
+let counter = 10;
+console.log(`Counter is currently ${counter}`);
+let updatedCounter = ++counter;
+console.log(`Counter is now ${updatedCounter}`);
+
+let score = 8;
+console.log(`Score is currently ${score}`);
+
+let finalScore = score++;
+console.log(`Score is still ${finalScore}`);
+
+let coins = 3;
+console.log(`Coins is currently ${coins}`);
+
+let updatedCoins = --coins;
+console.log(`Coins is now ${updatedCoins}`);
+
+let health = 7;
+console.log(`Health is currently ${health}`);
+
+let newHealth = health--;
+console.log(`Health is still ${newHealth}`);
+
+const hasDeveloperJob = true;
+
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+
+const isTimmyAGamer = false;
+
+if (isTimmyAGamer) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
+
+const timmyAge = 13;
+
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+} else {
+  console.log("Timmy is not old enough to drive.");
+}
