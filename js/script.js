ddb8a3e19632d942ab113588b78b06e79c9b08ac
canolidev <canovoti@gmail.com>
2026-06-10 14:47:00 -0500

    c5 stp 33 de 41

--- js/script.js
+++ js/script.js
@@ -1589,3 +1589,53 @@ aclaracion 6 horas de estudio diario ,
 
   
 avanze hoy en teoria y aplique a gigcx, y saldre a trabjar un rarto para completar esos 45 servicios */
+__________________________________________________
+
+const recipes = [];
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+const recipe2 = {
+  name: "Chicken Curry",
+  ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+const recipe3 = {
+  name: "Vegetable Stir Fry",
+  ingredients: ["broccoli", "carrot", "bell pepper"],
+  cookingTime: 15,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+recipes.push(recipe1, recipe2, recipe3);
+
+function getTotalIngredients(ingredients) {
+  return ingredients.length;
+}
+
+function getDifficultyLevel(cookingTime) {
+  if (cookingTime <= 30) {
+    return "easy";
+  } else if (cookingTime <= 60) {
+    return "medium";
+  } else {
+    return "hard";
+  }
+}
+ 
+const  recipe1TotalIngredients  =getTotalIngredients(recipe1.ingredients);
+
+const  recipe1DifficultyLevel  =getDifficultyLevel(recipe1.difficultyLevel) ;
+
+console.log(recipe1TotalIngredients)
+console.log(recipe1DifficultyLevel)
